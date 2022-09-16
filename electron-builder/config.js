50f8b13d833a562ce0e8890bd1eed75a47b1c273
emmaxoda <emanuelxoda@gmail.com>
2022-09-16 01:29:50 -0300

    building deb and rpm packages

--- electron-builder/config.js
+++ electron-builder/config.js
@@ -17,7 +17,9 @@ module.exports = {
         publish: null,
         target: [
             "AppImage",
-            "snap"
+            "snap",
+            "deb",
+            "rpm"
         ]
     },
     protocols: [{
